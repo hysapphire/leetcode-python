7525c688c27d2d3cbd1f9538371a0c5bdc9fbc29
huoyang <huoyang93@qq.com>
2020-07-12 14:07:19 +0800

    200712

--- src/162.寻找峰值.py
+++ src/162.寻找峰值.py
@@ -7,11 +7,32 @@
 # @lc code=start
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        nums.insert(0, -float("inf"))
-        nums.append(-float("inf"))
-        for i in range(1, len(nums) - 1):
-            if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
-                return i - 1
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
+
+
+
+
+        # for i in range(len(nums) - 1):
+        #     if nums[i] > nums[i+1]:
+        #         return i
+        
+        # return len(nums) - 1
+
+
+        # nums.insert(0, -float("inf"))
+        # nums.append(-float("inf"))
+        # for i in range(1, len(nums) - 1):
+        #     if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
+        #         return i - 1
         
 # @lc code=end
 
